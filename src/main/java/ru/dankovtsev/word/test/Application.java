80ec8c8fff683c1374f52759101fd4bd1a3cb102
Vldaislav_19 <vladislav.dankovtsev@yandex.ru>
2020-10-14 18:39:00 +0300

    first commit

---
+++ src/main/java/ru/dankovtsev/word/test/Application.java
@@ -0,0 +1,101 @@
+package ru.dankovtsev.word.test;
+
+import org.apache.poi.util.Units;
+import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTP;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSectPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTText;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class Application {
+    public static void main(String[] args) {
+        try {
+            // создаем модель docx документа,
+            // к которой будем прикручивать наполнение (колонтитулы, текст)
+            XWPFDocument docxModel = new XWPFDocument();
+            CTSectPr ctSectPr = docxModel.getDocument().getBody().addNewSectPr();
+            // получаем экземпляр XWPFHeaderFooterPolicy для работы с колонтитулами
+            XWPFHeaderFooterPolicy headerFooterPolicy = new XWPFHeaderFooterPolicy(docxModel, ctSectPr);
+
+            // создаем верхний колонтитул Word файла
+            CTP ctpHeaderModel = createHeaderModel(
+                    "Max инженер)"
+            );
+            // устанавливаем сформированный верхний
+            // колонтитул в модель документа Word
+            XWPFParagraph headerParagraph = new XWPFParagraph(ctpHeaderModel, docxModel);
+            headerFooterPolicy.createHeader(
+                    XWPFHeaderFooterPolicy.DEFAULT,
+                    new XWPFParagraph[]{headerParagraph}
+            );
+
+            // создаем нижний колонтитул docx файла
+            CTP ctpFooterModel = createFooterModel("Или нет");
+            // устанавливаем сформированый нижний
+            // колонтитул в модель документа Word
+            XWPFParagraph footerParagraph = new XWPFParagraph(ctpFooterModel, docxModel);
+            headerFooterPolicy.createFooter(
+                    XWPFHeaderFooterPolicy.DEFAULT,
+                    new XWPFParagraph[]{footerParagraph}
+            );
+
+            // создаем обычный параграф, который будет расположен слева,
+            // будет синим курсивом со шрифтом 25 размера
+            XWPFParagraph bodyParagraph = docxModel.createParagraph();
+            bodyParagraph.setAlignment(ParagraphAlignment.RIGHT);
+            XWPFRun paragraphConfig = bodyParagraph.createRun();
+            //paragraphConfig.setItalic(true);
+            paragraphConfig.setFontSize(25);
+            // HEX цвет без решетки #
+            paragraphConfig.setColor("06357a");
+            paragraphConfig.setText(
+                    "Создание документа Word (.docx) с помощью Apache POI. Теория"
+            );
+            File file = new File("kvi.jpg");
+            BufferedImage img = ImageIO.read(file);
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            ImageIO.write(img, "jpg", os);
+            InputStream imgFile = new ByteArrayInputStream(os.toByteArray());;
+            paragraphConfig.addPicture(imgFile, XWPFDocument.PICTURE_TYPE_PNG, "out.png", Units.toEMU(500), Units.toEMU(500));
+
+            InputStream imgFile2 = new ByteArrayInputStream(os.toByteArray());;
+            paragraphConfig.addPicture(imgFile2, XWPFDocument.PICTURE_TYPE_PNG, "out.png", Units.toEMU(500), Units.toEMU(500));
+
+            // сохраняем модель docx документа в файл
+            FileOutputStream outputStream = new FileOutputStream("Test.docx");
+            docxModel.write(outputStream);
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Успешно записан в файл");
+    }
+
+    private static CTP createFooterModel(String footerContent) {
+        // создаем футер или нижний колонтитул
+        CTP ctpFooterModel = CTP.Factory.newInstance();
+        CTR ctrFooterModel = ctpFooterModel.addNewR();
+        CTText cttFooter = ctrFooterModel.addNewT();
+
+        cttFooter.setStringValue(footerContent);
+        return ctpFooterModel;
+    }
+
+    private static CTP createHeaderModel(String headerContent) {
+        // создаем хедер или верхний колонтитул
+        CTP ctpHeaderModel = CTP.Factory.newInstance();
+        CTR ctrHeaderModel = ctpHeaderModel.addNewR();
+        CTText cttHeader = ctrHeaderModel.addNewT();
+
+        cttHeader.setStringValue(headerContent);
+        return ctpHeaderModel;
+    }
+}
